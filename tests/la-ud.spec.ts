b1ca5da67332cf5b5a605905f57aef51b43360a5
Alexander Petrovskiy <uiautomation@hotmail.com>
2022-04-21 18:13:06 +0300

    plaintiff and defendant

--- tests/la-ud.spec.ts
+++ tests/la-ud.spec.ts
@@ -9,13 +9,18 @@ test.beforeEach(async ({ page }) => {
 });
 
 // TODO: to test data
+const disputeNumber = '29STUD45011';
+
 const plaintiffName = 'pla001@rambler.ru';
 const plaintiffPassword = 'Lock12Lock';
 const plaintiffId = '17698209';
-const disputeNumber = '29STUD45011';
+
+const defendantName = 'defe001@rambler.ru';
+const defendantsPassword = 'Lock12Lock';
+const defendantId = '17698208';
 
 test.describe('LA UD start', () => {
-  test('should login into ODR', async ({ page }) => {
+  test('should login into ODR as a plaintiff SRL', async ({ page }) => {
     // const { chromium } = require('playwright');
     // const browser = await chromium.launch()
     // const page = await browser.newPage()
@@ -34,7 +39,27 @@ test.describe('LA UD start', () => {
 
     await openCase(page, openCaseData);
 
-    await page.pause();
+    // await page.pause();
+
+    // await browser.close()
+    await page.close();
+  });
+
+  test('should login into ODR as a defendant SRL', async ({ page }) => {
+    // TODO: to config
+    await page.setViewportSize({ width: 2655, height: 1361 });
+
+    const openCaseData: OpenCaseDto = {
+      email: defendantName,
+      password: defendantsPassword,
+      id: defendantId,
+      location: Locations.stanleyMosk,
+      disputeNumber: disputeNumber,
+    };
+
+    await openCase(page, openCaseData);
+
+    // await page.pause();
 
     // await browser.close()
     await page.close();
